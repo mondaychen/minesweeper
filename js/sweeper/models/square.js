c4ff9b96855cb56c32acbfab2f4fea42002ffd41
mondaychen <monday.chen@gmail.com>
2015-03-10 21:06:14 -0400

    fix game logic

--- js/sweeper/models/square.js
+++ js/sweeper/models/square.js
@@ -23,7 +23,7 @@ define([
       }
       this.set('isOpen', true)
       if(!this.get('isMine') && this.get('number') === 0) {
-        var neighbours = this.collection.matrix.getNeighbour4ByIdx(this.get('index'))
+        var neighbours = this.collection.matrix.getNeighbour8ByIdx(this.get('index'))
         _.each(neighbours, function(neighbour) {
           var model = self.collection.getModelByPosition(neighbour)
           model.open()
